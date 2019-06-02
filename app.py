ac14a4a2d1cd60c8494f55d8429cbf83752a3ea2
rafirahim <rafipattazhi123@gmail.com>
2019-06-02 14:30:55 +0530

    student list

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask,render_template
+from data import Students
+
 app=Flask(__name__)
+
+stud=Students()
+
+
 @app.route('/index')
 def index():
-    return  render_template('index.html')
+    return  render_template('index.html',studList=stud)
 
 @app.route('/home')
 def home():
--- data.py
+++ data.py
@@ -1,34 +1,35 @@
 def Students():
     students=[
         {
-            "name":"Rahul;",
+            "name":"Rahul",
             "age":23,
             "admno":2234,
             "parentname":"raju"
         },
         {
-            "name":"Tom;",
+            "name":"Tom",
             "age":44,
             "admno":2278,
             "parentname":"Eric"
         },
         {
-            "name":"Siva;",
+            "name":"Siva",
             "age":22,
             "admno":2220,
             "parentname":"Aju"
         },
         {
-            "name":"Abin;",
+            "name":"Abin",
             "age":20,
             "admno":2239,
             "parentname":"Abraham"
         },
         {
-            "name":"Kiran;",
+            "name":"Kiran",
             "age":25,
             "admno":2269,
             "parentname":"Kumar"
         }
          
     ]
+    return students
